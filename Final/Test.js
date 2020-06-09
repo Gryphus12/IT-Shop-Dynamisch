5ef4367aa40cd540a92dd8b4a5ed4290cd656e93
Gryphus12 <60388052+Gryphus12@users.noreply.github.com>
2020-06-09 21:04:35 +0200

    Aktueller Stand

---
+++ Final/Test.js
@@ -0,0 +1,31 @@
+// JSON Objecte aus den eingegeben Daten an die API übergeben
+
+function postAddress(){
+    const contact = {
+        name: document.getElementById('name').value,
+        absender: document.getElementById('absender').value,
+        nachricht: document.getElementById('nachricht').value
+    };
+
+    //Adressdaten übertragen
+
+    fetch(baseApiUrl + 'adresse',{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(contact),
+    })
+    .then((response) => {
+        response.json().then((json) => {
+            if (json.fehler) console.error('Error im backend: ' + json.nachricht);
+            else{
+                contact.adresse = {id: json.daten.id};
+                postAddition(contact);
+            }
+        });
+    })
+    .catch((err) => {
+        console.error('Error while API-Call: ' + err);
+    });
+}
